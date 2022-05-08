d32b0150d2731b4b538d2be4e154039563249b8b
Sgobbi Federico <federico@sgobbi.it>
2022-05-08 23:00:13 +0200

    INIT repository

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nodejs hass client
+Nodejs websocket client for Home Assistant.
+
+### Install
+```
+npm install @digidockage/node-hass-client
+```
---
+++ src/index.js
@@ -0,0 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                                 *
+ *    Copyright (c) 2022 Sgobbi Federico                                                           *
+ *    All rights reserved.                                                                         *
+ *                                                                                                 *
+ *    This file is licensed under the MIT License.                                                 *
+ *    License text available at https://opensource.org/licenses/MIT                                *
+ *                                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// > > > > > > > > > > > > > > > > > > > > > > > The code
+const HassClient = () => {
+  console.log('Hello world!');
+};
+
+// > > > > > > > > > > > > > > > > > > > > > > > Module exports
+module.exports = HassClient;
